6c601c7a94caf874c0b403139a5cf5b2cfd36442
Ian Brandt <ian@ianbrandt.com>
2021-02-04 21:59:52 -0800

    Initial Gilded Rose kata commit.

---
+++ gilded-rose-1/src/main/kotlin/com/ianbrandt/kata/Item.kt
@@ -0,0 +1,3 @@
+package com.ianbrandt.kata
+
+data class Item(var name: String, var sellIn: Int, var quality: Int)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,5 @@
 rootProject.name = "kotlin-coding-katas"
 
 include(":fizz-buzz-1")
+include(":gilded-rose-1")
 include(":roman-numerals-1")
