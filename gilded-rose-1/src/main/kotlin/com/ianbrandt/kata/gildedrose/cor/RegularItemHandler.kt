dcbae2417671bb9eede8426542c65f4f6bec1851
Ian Brandt <ian@ianbrandt.com>
2021-02-07 11:10:20 -0800

    Clarified RegularItemHandler as a terminal handler.

--- gilded-rose-1/src/main/kotlin/com/ianbrandt/kata/gildedrose/cor/RegularItemHandler.kt
+++ gilded-rose-1/src/main/kotlin/com/ianbrandt/kata/gildedrose/cor/RegularItemHandler.kt
@@ -1,16 +1,17 @@
 package com.ianbrandt.kata.gildedrose.cor
 
 import com.ianbrandt.cor.BaseHandler
-import com.ianbrandt.cor.Handler
 import com.ianbrandt.kata.gildedrose.Item
 import com.ianbrandt.kata.gildedrose.cor.ItemHandlerUtils.decreaseExpiredQuality
 import com.ianbrandt.kata.gildedrose.cor.ItemHandlerUtils.decreaseQuality
 import com.ianbrandt.kata.gildedrose.cor.ItemHandlerUtils.decreaseSellIn
 
-internal class RegularItemHandler(
-		nextHandler: Handler<Item>? = null
-) : BaseHandler<Item>(nextHandler) {
+internal class RegularItemHandler : BaseHandler<Item>(
+		// Terminal handler
+		nextHandler = null
+) {
 
+	// Catch-all
 	override fun canHandle(item: Item) = true
 
 	override fun doHandle(item: Item) {
